ee388dd5dd0107e54ed00e61b590aa1fbda89816
Maram <Maram@Maram-PC>
2016-11-16 14:15:09 +0530

    test changes

--- sample/src/main/java/com/springboottest/TestRootControllerOne.java
+++ sample/src/main/java/com/springboottest/TestRootControllerOne.java
@@ -1,20 +1,36 @@
 package com.springboottest;
 
+import java.util.ArrayList;
+
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.testcontroller.TestEntityOne;
+
 @RestController
 @EnableAutoConfiguration
+@ComponentScan({ "com.testcontroller"})
 public class TestRootControllerOne {
+	
+	private ArrayList<TestEntityOne> teoList;
 	
 	@RequestMapping("/")
 	public String getData(){
 		return "Hello";
 	}
 	
+	public ArrayList<TestEntityOne> getTeoList() {
+		return teoList;
+	}
+
+	public void setTeoList(ArrayList<TestEntityOne> teoList) {
+		this.teoList = teoList;
+	}
+
 	@RequestMapping("/test")
-	public String getTest(){
-		return "check";
+	public ArrayList<TestEntityOne> getTest(){
+		return teoList;
 	}
 }
