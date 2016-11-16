3ef284349e4c4fc125597eaa2a48266e3ec69523
Maram <Maram@Maram-PC>
2016-11-16 14:58:55 +0530

    push

--- sample/src/main/java/com/testcontroller/TestRootController.java
+++ sample/src/main/java/com/testcontroller/TestRootController.java
@@ -3,7 +3,6 @@ package com.testcontroller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
