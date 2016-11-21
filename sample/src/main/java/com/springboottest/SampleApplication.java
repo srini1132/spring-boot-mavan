0db677315235df91f2e6d48f163b214b7d4a750b
Maram <Maram@Maram-PC>
2016-11-22 03:41:09 +0530

    oAuth Success

--- sample/src/main/java/com/springboottest/SampleApplication.java
+++ sample/src/main/java/com/springboottest/SampleApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({ "com.springboottest", "com.testcontroller"})
+@ComponentScan({ "com.springboottest", "com.testcontroller","com.srini.oauth"})
 public class SampleApplication {
 
 	public static void main(String[] args) {
---
+++ sample/src/main/java/com/srini/oauth/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.srini.oauth;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
